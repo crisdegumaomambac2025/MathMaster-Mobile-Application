c7cae3b235a008d194a0236fc01aae8e1fe38834
Cris Omambac <cris.omambac@gws.ctu.edu.ph>
2025-05-31 23:45:44 +0800

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using MathMaster.View;
+using MathMaster.Services;
+using Microsoft.Maui.Controls;
+
+namespace MathMaster
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+            //return new Window(new NavigationPage(new WelcomePage()));
+        }
+
+        protected override void OnSleep()
+        {
+            base.OnSleep();
+            BackgroundMusicService.Instance.Stop();
+        }
+
+        protected override async void OnResume()
+        {
+            base.OnResume();
+            await BackgroundMusicService.Instance.ResumeLastMusicIfAny();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MathMaster
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
